0e14ee012412837521bc54d902c29a2b6e460fdc
Alexandru Ionut Balan <alexandru.ionut.balan@proton.me>
2023-05-31 14:36:35 +0300

    feat: Add logging

---
+++ core/log/log.go
@@ -0,0 +1,10 @@
+package log
+
+import (
+	"log"
+	"os"
+)
+
+var Info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+var Warn = log.New(os.Stdout, "WARN: ", log.Ldate|log.Ltime|log.Lshortfile)
+var Error = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
