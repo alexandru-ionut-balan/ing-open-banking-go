f60624d9bd7d4241d9511945d0646295ae0f62e7
Alexandru Ionut Balan <alexandru.ionut.balan@proton.me>
2023-05-31 16:20:38 +0300

    feat: Add signature header

--- clients/headers.go
+++ clients/headers.go
@@ -2,7 +2,10 @@ package clients
 
 import (
 	"crypto"
+	"crypto/rsa"
+	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/alexandru-ionut-balan/ing-open-banking-go/core"
@@ -23,4 +26,39 @@ func Date(header *http.Header) {
 	date := time.Now().In(location).Format(time.RFC1123)
 
 	header.Add("Date", date)
+}
+
+func Signature(header http.Header, keyId string, algorithm crypto.Hash, privateKey *rsa.PrivateKey) {
+	var headerNames string
+	var signaturePayload string
+	var signatureAlgorithm string
+
+	for headerName, headerValue := range header {
+		var headerValueString string
+		for _, item := range headerValue {
+			headerValueString += item + ","
+		}
+		headerValueString = strings.TrimSuffix(headerValueString, ",")
+
+		lowerHeaderName := strings.ToLower(headerName)
+
+		headerNames += lowerHeaderName + " "
+		signaturePayload += lowerHeaderName + ": " + headerValueString + "\n"
+	}
+
+	headerNames = strings.TrimSuffix(headerNames, " ")
+	signaturePayload = strings.TrimSuffix(signaturePayload, "\n")
+
+	switch algorithm {
+	case crypto.SHA512:
+		signatureAlgorithm = "rsa-sha512"
+	default:
+		signatureAlgorithm = "rsa-sha256"
+	}
+
+	signedPayload := encode.Sign(signaturePayload, algorithm, privateKey)
+
+	signature := fmt.Sprintf("keyId=\"SN=%s\",algorithm=\"%s\",header\"%s\",signature=\"%s\"", keyId, signatureAlgorithm, headerNames, signedPayload)
+
+	header.Add("Signature", signature)
 }
